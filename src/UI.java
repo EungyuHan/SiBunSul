e819228aeb82e3509cb6d1ca42f023e5c26b54ed
Haneungyu <105436435+EungyuHan@users.noreply.github.com>
2023-11-28 20:03:31 +0900

    remove: remove unusable JLabel

--- src/UI.java
+++ src/UI.java
@@ -11,9 +11,9 @@ public class UI implements ActionListener {
 	UI() {  
 		
 		/* ***************************** Setting Up J-Elements ***************************** */
-		uif.panel.setLayout(null);
+		uif.mainPanel.setLayout(null);
 
-		uif.panel.setBounds(500, 50, 500, 600);
+		uif.mainPanel.setBounds(500, 50, 500, 600);
 		uif.trainerLabel.setBounds(50,50,500,30);
 		uif.trainerTextField.setBounds(50,100,200,20);
 		uif.memberLabel.setBounds(50,150,500,30);
@@ -21,117 +21,37 @@ public class UI implements ActionListener {
 		uif.searchTrainerButton.setBounds(275,100,125,20);
 		uif.searchMemberButton.setBounds(275,200,125,20);
 
-		uif.nameLabel.setBounds(500,50,300,30);
-		uif.nameLabel.setFont(new Font("TimesRoman", Font.BOLD, 20));
-		uif.emailLabel.setBounds(500,100,500,30);
-		uif.phoneLabel.setBounds(500,150,500,30);
-		uif.majorLabel.setBounds(500,200,500,30);
-		uif.gpaLabel.setBounds(500,250,500,30);
-		uif.classYearLabel.setBounds(500,300,500,30);
-		uif.healthRecordLabel.setBounds(500,350,500,30);
-		uif.timeRecordLabel.setBounds(500,400,500,30);
-		uif.l11.setBounds(500,450,500,30);
-		uif.l12.setBounds(500,500,500,30);
-		uif.l13.setBounds(500,550,500,30);
-		uif.l14.setBounds(500,600,500,30);
-		uif.l15.setBounds(500,650,500,30);
-		uif.l16.setBounds(500,700,500,30);	
-		uif.l17.setBounds(625,100,500,30);
-		uif.l18.setBounds(625,150,500,30);
-		uif.l19.setBounds(625,200,500,30);
-		uif.l20.setBounds(625,250,500,30);
-		uif.l21.setBounds(625,300,500,30); 
-		uif.l22.setBounds(625,350,500,30); 
-		uif.l23.setBounds(625,400,500,30); 
-		uif.l24.setBounds(625,450,500,30);
-		uif.l25.setBounds(625,500,500,30); 
-		uif.l26.setBounds(625,550,500,30); 
-		uif.l27.setBounds(625,600,500,30); 
-		uif.l28.setBounds(625,650,500,30); 
-		uif.l29.setBounds(625,700,500,30); 
-		uif.l30.setBounds(750,100,500,30); 
-		uif.l31.setBounds(750,150,500,30); 
-		uif.l32.setBounds(750,200,500,30); 
-		uif.l33.setBounds(750,250,500,30); 
-		uif.l34.setBounds(750,300,500,30); 
-		uif.l35.setBounds(750,350,500,30); 
-		uif.l36.setBounds(750,400,500,30); 
-		uif.l37.setBounds(750,450,500,30); 
-		uif.l38.setBounds(750,500,500,30); 
-		uif.l39.setBounds(750,550,500,30); 
-		uif.l40.setBounds(750,600,500,30); 
-		uif.l41.setBounds(750,650,500,30); 
-		uif.l42.setBounds(750,700,500,30); 
-		uif.l43.setBounds(900,100,500,30); 
-		uif.l44.setBounds(900,150,500,30); 
-		uif.l45.setBounds(900,200,500,30); 
-		uif.l46.setBounds(900,250,500,30); 
-		uif.l47.setBounds(900,300,500,30); 
-		uif.l48.setBounds(900,350,500,30); 
-		uif.l49.setBounds(900,400,500,30); 
-		uif.l50.setBounds(900,450,500,30); 
-		uif.l51.setBounds(900,500,500,30); 
-		uif.l52.setBounds(900,550,500,30); 
-		uif.l53.setBounds(900,600,500,30); 
-		uif.l54.setBounds(900,650,500,30);		
-		uif.l55.setBounds(900,700,500,30); 
-
-
-		uif.tf2.setBounds(715,100,200,30);
-		uif.tf3.setBounds(715,150,200,30);  
-		uif.tf4.setBounds(715,200,200,30);  
-		uif.tf5.setBounds(715,250,200,30);  
-		uif.tf6.setBounds(715,300,200,30);
-
-		uif.buttonViewHealth.setBounds(600,350,100,30);
-		uif.buttonViewTime.setBounds(600,400,100,30);
-
 		uif.submitButton.setBounds(820,50,100,30);
 
-		uif.buttonViewTime.setVisible(false);
-		uif.buttonViewHealth.setVisible(false);
 		uif.submitButton.setVisible(false);
-		uif.tf2.setVisible(false);
-		uif.tf3.setVisible(false);
-		uif.tf4.setVisible(false);
-		uif.tf5.setVisible(false);
-		uif.tf6.setVisible(false);
-
 		// ***************************** Action Listener *****************************
-		uif.searchTrainerButton.addActionListener(new ActionListenerTrainerSearch(av.trainerList, uif.panel, uif.trainerTextField));
-		uif.searchMemberButton.addActionListener(new ActionListenerMemberSearch(av.trainerList, uif.panel, uif.trainerTextField ,uif.memberTextField));
+		uif.searchTrainerButton.addActionListener(new ActionListenerTrainerSearch(av.trainerList, uif.mainPanel, uif.trainerTextField));
+		uif.searchMemberButton.addActionListener(new ActionListenerMemberSearch(av.trainerList, uif.mainPanel, uif.trainerTextField ,uif.memberTextField));
 
 
 		uif.addTrainerMenu.addActionListener(new ActionListenerAddTrainer(av.trainerList));
 		uif.addMemberMenu.addActionListener(new ActionListenerAddMember(av.trainerList, av.memberList));
 
-		uif.viewMemberList.addActionListener(new ActionListenerViewMemberList(uif.panel, av.memberList, av.trainerList));
-		uif.viewTrainerList.addActionListener(new ActionListenerViewTrainerList(uif.panel, av.memberList, av.trainerList));
+		uif.viewTrainerList.addActionListener(new ActionListenerViewTrainerList(uif.mainPanel, av.memberList, av.trainerList));
+		uif.viewMemberList.addActionListener(new ActionListenerViewMemberList(uif.mainPanel, av.memberList, av.trainerList));
 
 		// i9 : 화면 제거
-		uif.clearButton.addActionListener(new ActionListenerClearView(uif.panel));
+		uif.clearButton.addActionListener(new ActionListenerClearView(uif.mainPanel));
 		// ***************************** Add J-Elements to Frame *****************************
 		
 		uif.addMenu.add(uif.addTrainerMenu); uif.addMenu.add(uif.addMemberMenu);
 		uif.viewMenu.add(uif.viewMemberList); uif.viewMenu.add(uif.viewTrainerList); uif.clearMenu.add(uif.clearButton);
 		
 		uif.mb.add(uif.addMenu);
-		uif.mb.add(uif.editMenu);
 		uif.mb.add(uif.viewMenu);
 		uif.mb.add(uif.clearMenu);
 		
 		uif.f.add(uif.searchTrainerButton); uif.f.add(uif.submitButton);
-		uif.panel.add(new JLabel("TEST"));
-		uif.f.add(uif.panel);
-		uif.panel.setVisible(true);
-		uif.f.add(uif.trainerLabel); uif.f.add(uif.nameLabel); uif.f.add(uif.temp); uif.f.add(uif.emailLabel); uif.f.add(uif.phoneLabel); uif.f.add(uif.majorLabel); uif.f.add(uif.gpaLabel); uif.f.add(uif.classYearLabel); uif.f.add(uif.healthRecordLabel); uif.f.add(uif.timeRecordLabel);
-		uif.f.add(uif.l11); uif.f.add(uif.l12); uif.f.add(uif.l13); uif.f.add(uif.l14); uif.f.add(uif.l15); uif.f.add(uif.l16); uif.f.add(uif.l17); uif.f.add(uif.l18); uif.f.add(uif.l19); uif.f.add(uif.l20);
-		uif.f.add(uif.l21); uif.f.add(uif.l22); uif.f.add(uif.l23); uif.f.add(uif.l24); uif.f.add(uif.l25); uif.f.add(uif.l26); uif.f.add(uif.l27); uif.f.add(uif.l28); uif.f.add(uif.l29); uif.f.add(uif.l30); 
-		uif.f.add(uif.l31); uif.f.add(uif.l32); uif.f.add(uif.l33); uif.f.add(uif.l34); uif.f.add(uif.l35); uif.f.add(uif.l36); uif.f.add(uif.l37); uif.f.add(uif.l38); uif.f.add(uif.l39); uif.f.add(uif.l40); 
-		uif.f.add(uif.l41); uif.f.add(uif.l42); uif.f.add(uif.l43); uif.f.add(uif.l44); uif.f.add(uif.l45); uif.f.add(uif.l46); uif.f.add(uif.l47); uif.f.add(uif.l48); uif.f.add(uif.l49); uif.f.add(uif.l50); 
-		uif.f.add(uif.l51); uif.f.add(uif.l52); uif.f.add(uif.l53); uif.f.add(uif.l54); uif.f.add(uif.l55); 
-		uif.f.add(uif.buttonViewHealth); uif.f.add(uif.buttonViewTime); 
-		uif.f.add(uif.trainerTextField); uif.f.add(uif.tf2); uif.f.add(uif.tf3); uif.f.add(uif.tf4); uif.f.add(uif.tf5); uif.f.add(uif.tf6); //uif.f.add(tf7); uif.f.add(tf8);
+		uif.mainPanel.add(new JLabel("TEST"));
+		uif.f.add(uif.mainPanel);
+		uif.mainPanel.setVisible(true);
+		uif.f.add(uif.trainerLabel);
+		uif.f.add(uif.trainerTextField);
 		uif.f.add(uif.memberLabel); uif.f.add(uif.memberTextField); uif.f.add(uif.searchMemberButton);
 
 		uif.f.setJMenuBar(uif.mb); 
--- src/UIFrame.java
+++ src/UIFrame.java
@@ -5,92 +5,25 @@ import javax.swing.*;
 public class UIFrame {
 	
 	public JFrame f = new JFrame("Application");  
-	public JMenuBar mb=new JMenuBar(); 
+	public JMenuBar mb=new JMenuBar();
+	public JPanel mainPanel = new JPanel();
     
 	public JMenu addMenu =new JMenu("Add");
-	public JMenu editMenu =new JMenu("Edit");
 	public JMenu viewMenu =new JMenu("View");
 	public JMenu clearMenu =new JMenu("Clear");
 
 	public JMenuItem addTrainerMenu = new JMenuItem("Add Trainer");
 	public JMenuItem addMemberMenu = new JMenuItem("Add Member");
-	public JMenuItem editTrainerMenu =new JMenuItem("Edit Trainer");
-	public JMenuItem editMemberMenu =new JMenuItem("Edit Member");
 	public JMenuItem viewMemberList =new JMenuItem("View Member");
 	public JMenuItem viewTrainerList =new JMenuItem("View Trainer");
 	public JMenuItem clearButton =new JMenuItem("Clear View");
 	
 	public JLabel trainerLabel =new JLabel("Welcome! Search for a Trainer here:");
 	public JLabel memberLabel =new JLabel("Search for a Member here:");
-	public JLabel nameLabel =new JLabel();
-	public JLabel temp = new JLabel();
-	public JPanel panel = new JPanel();
-	public JLabel emailLabel = new JLabel(); 
-	public JLabel phoneLabel = new JLabel();
-	public JLabel majorLabel = new JLabel();
-	public JLabel gpaLabel = new JLabel();
-	public JLabel classYearLabel = new JLabel();
-	public JLabel healthRecordLabel = new JLabel();
-	public JLabel timeRecordLabel = new JLabel();
-	public JLabel l11 = new JLabel();
-	public JLabel l12 = new JLabel(); 
-	public JLabel l13 = new JLabel();
-	public JLabel l14 = new JLabel();
-	public JLabel l15 = new JLabel(); 
-	public JLabel l16 = new JLabel();
-	public JLabel l17 = new JLabel();
-	public JLabel l18 = new JLabel();
-	public JLabel l19 = new JLabel();
-	public JLabel l20 = new JLabel();
-	public JLabel l21 = new JLabel(); 
-	public JLabel l22 = new JLabel(); 
-	public JLabel l23 = new JLabel(); 
-	public JLabel l24 = new JLabel();
-	public JLabel l25 = new JLabel(); 
-	public JLabel l26 = new JLabel(); 
-	public JLabel l27 = new JLabel(); 
-	public JLabel l28 = new JLabel(); 
-	public JLabel l29 = new JLabel(); 
-	public JLabel l30 = new JLabel(); 
-	public JLabel l31 = new JLabel(); 
-	public JLabel l32 = new JLabel(); 
-	public JLabel l33 = new JLabel(); 
-	public JLabel l34 = new JLabel(); 
-	public JLabel l35 = new JLabel(); 
-	public JLabel l36 = new JLabel(); 
-	public JLabel l37 = new JLabel(); 
-	public JLabel l38 = new JLabel(); 
-	public JLabel l39 = new JLabel(); 
-	public JLabel l40 = new JLabel(); 
-	public JLabel l41 = new JLabel(); 
-	public JLabel l42 = new JLabel(); 
-	public JLabel l43 = new JLabel(); 
-	public JLabel l44 = new JLabel(); 
-	public JLabel l45 = new JLabel(); 
-	public JLabel l46 = new JLabel(); 
-	public JLabel l47 = new JLabel(); 
-	public JLabel l48 = new JLabel(); 
-	public JLabel l49 = new JLabel(); 
-	public JLabel l50 = new JLabel(); 
-	public JLabel l51 = new JLabel(); 
-	public JLabel l52 = new JLabel(); 
-	public JLabel l53 = new JLabel(); 
-	public JLabel l54 = new JLabel();		
-	public JLabel l55 = new JLabel(); 
-	
-	public JButton buttonViewHealth = new JButton();
-	public JButton buttonViewTime = new JButton();
-			
-    public JTextField trainerTextField =new JTextField();
+	public JTextField trainerTextField =new JTextField();
 	public JTextField memberTextField =new JTextField();
-    public JTextField tf2=new JTextField();
-    public JTextField tf3=new JTextField();
-    public JTextField tf4=new JTextField();
-    public JTextField tf5=new JTextField();
-    public JTextField tf6=new JTextField();
-    
-    public JButton searchTrainerButton =new JButton("Search Trainer");
+	public JButton searchTrainerButton =new JButton("Search Trainer");
 	public JButton searchMemberButton =new JButton("Search Member");
-    public JButton submitButton =new JButton("Submit");
-	
+	public JButton submitButton =new JButton("Submit");
+
 }
